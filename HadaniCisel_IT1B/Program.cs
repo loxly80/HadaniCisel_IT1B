5db2207a88954b236cd7876b10df840341531b9e
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-11-16 11:34:36 +0100

    Hotovo

--- HadaniCisel_IT1B/Program.cs
+++ HadaniCisel_IT1B/Program.cs
@@ -12,12 +12,24 @@ namespace HadaniCisel_IT1B
             bool neuhodl = true;
             Console.WriteLine(" Hadání čísla");
             Console.WriteLine("---------------------");
-            Console.WriteLine($" Myslím si číslo od 1 do 100");
+            Console.WriteLine($" Myslím si číslo od 1 do 100 {nahCislo}");
             while (neuhodl)
             {
                 Console.Write(" Tipni hodnotu: ");
                 tipCislo = int.Parse(Console.ReadLine());
-
+                if (tipCislo > nahCislo)
+                {
+                    Console.WriteLine(" To je moc");
+                }
+                else if (tipCislo < nahCislo)
+                {
+                    Console.WriteLine(" To je málo");
+                }
+                else
+                {
+                    Console.WriteLine(" Trefa");
+                    neuhodl = false;
+                }
             }
             Console.ReadLine();
         }

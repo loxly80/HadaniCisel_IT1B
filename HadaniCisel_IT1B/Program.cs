2214bcf694c80c9917b3a4b2f91a76a7069aeb07
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-11-16 10:42:08 +0100

    Vygenerování čísla

--- HadaniCisel_IT1B/Program.cs
+++ HadaniCisel_IT1B/Program.cs
@@ -6,7 +6,13 @@ namespace HadaniCisel_IT1B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int nahCislo = random.Next(1,100);
+            Console.WriteLine(" Hadání čísla");
+            Console.WriteLine("---------------------");
+            Console.WriteLine($" Myslím si číslo od 1 do 100");
+
+            Console.ReadLine();
         }
     }
 }
